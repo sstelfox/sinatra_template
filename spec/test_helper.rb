d95703bf30c3e3e8dab0e5e2c4bc63a3c9845aeb
Jeremy Waters <jwaters@0x378.net>
2013-08-27 00:11:44 -0400

    Fleshed out test case for a little bit more

--- spec/test_helper.rb
+++ spec/test_helper.rb
@@ -4,3 +4,18 @@ ENV['RACK_ENV'] = 'test'
 require 'minitest/autorun'
 require 'rack/test'
 
+# The base for a test case, all minitest cases under this directory should sub
+# class this.
+class TestCase < MiniTest::Unit::TestCase
+  include Rack::Test::Methods
+
+  def app
+    Default::App
+  end
+end
+
+spec_dir = File.dirname(__FILE__)
+Dir.glob(File.expand_path(File.join(spec_dir, '**', "*_spec.rb"))).each do |f|
+  require f
+end
+
